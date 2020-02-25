ac002a1015389fe15e55db6b19ac65d01d2abf9d
Derek Miranda <derek.miranda.1992@gmail.com>
2020-02-24 16:06:11 -0800

    Adds bt-redist-push script

---
+++ bt-redist-push.sh
@@ -0,0 +1,17 @@
+# runs C2.0 Build Template redist, commit, and push
+# use from BT directory
+function bt_redist_push() {
+  msg="$1"
+
+  npm run c20
+  git add -A .
+  git commit -m "$msg"
+  git push
+}
+
+for bt in "bt-*"
+do
+  cd "$bt"
+  bt_redist_push "$1"
+  cd ..
+done
